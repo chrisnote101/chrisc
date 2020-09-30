8a4625ab6aef319979aa75cc4ec0d269ab15957c
chrisnote101 <71943715+chrisnote101@users.noreply.github.com>
2020-09-30 09:35:53 +0200

    Update hw.php

--- hw.php
+++ hw.php
@@ -5,7 +5,9 @@ print("Hello world");
 
 ?>
 <SCRIPT>
-  var _url = 'https://prod-48.westeurope.logic.azure.com:443/workflows/78f270d5827e453587e39b92d460edb8/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=s8PXNv0R4mvJkJ91sNoKUm2FVisIkfcQyuVDd1wWLGw';
+//  var _url = 'https://prod-48.westeurope.logic.azure.com:443/workflows/78f270d5827e453587e39b92d460edb8/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=s8PXNv0R4mvJkJ91sNoKUm2FVisIkfcQyuVDd1wWLGw';
+  https://chrisc.westeurope-1.eventgrid.azure.net/api/events
+  var _url = 'https://chrisc.westeurope-1.eventgrid.azure.net/api/events';
   
  // var _url = ' https://prod-86.westeurope.logic.azure.com:443/workflows/6557518def5b46fc9fc2060d377f13ba/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=KLRutX6qXTPtOHsBogYXBhalY3Yopz3A6vVyXQhlGXE";
 // var _url = 'https://s4events.azure-automation.net/webhooks?token=your_token';
