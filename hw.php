52f3d1cb6a1b54dc67573e8ef5135eea001e7f65
chrisnote101 <71943715+chrisnote101@users.noreply.github.com>
2020-09-30 20:33:36 +0200

    Update hw.php

--- hw.php
+++ hw.php
@@ -1,6 +1,6 @@
 
 <?PHP
-$ver="1.2";
+$ver="1.3";
 //header("Access-Control-Allow-Origin: https://chrisc.westeurope-1.eventgrid.azure.net");
 //header("Access-Control-Allow-Origin: https://chrisc.westeurope-1.eventgrid.azure.net/*");
 print("Hello world 2<br>");
@@ -37,7 +37,7 @@ var xhttp=new XMLHttpRequest();
          };
     xhttp.open("POST", "https://chrisc.westeurope-1.eventgrid.azure.net/api/events?aeg-sas-key=SdIEB8CxiY6LuZcoMUGrxh+gGXXCbUtClkn5BuSQ5lI=", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.setRequestHeader("Access-Control-Allow-Origin: https://chrisc.westeurope-1.eventgrid.azure.net");
+    xhttp.setRequestHeader("Access-Control-Allow-Origin", "https://chrisc.westeurope-1.eventgrid.azure.net");
     xhttp.send(JSON.stringify(a));
 }
 submitOrder();
