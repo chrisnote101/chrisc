4c0b292f0803aeb0dd6aa0a2edea52108368350e
chrisnote101 <71943715+chrisnote101@users.noreply.github.com>
2020-10-06 11:53:03 +0200

    Update hw.php

--- hw.php
+++ hw.php
@@ -18,10 +18,11 @@ class myObj {
 //$myObj->eventType="started";
 //$myObj->subject="hits";
 //$myObj->eventTime="2020-10-06T11:03:07+00:00";
-$ee = array('id' => '1087',  'eventType'  => 'Start',  'subject'    => 'hits');
+//$ee = array('id' => '1087',  'eventType'  => 'Start',  'subject'    => 'hits');
+$ee = array('id' => '1089');
 
 //$ee=json_encode($myObj,true);
-$ver="1.7";
+$ver="1.8";
 print("Hello world <br>");
 print("Complete ".$ver."<br>");
 $aeg="EQ3xDFMMJlW3rl+tyug6kVyzrYmjh1U0qEalJmQ/bc4=";
