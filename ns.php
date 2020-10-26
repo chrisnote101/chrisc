f7a72d4d09af387d22700363a506b6f559e20fed
chrisnote101 <71943715+chrisnote101@users.noreply.github.com>
2020-10-26 13:33:41 +0100

    Update ns.php

--- ns.php
+++ ns.php
@@ -3,9 +3,9 @@ class myObj {
     public $code;
     public $text;
 }
-$ee = "{ 'name': 'abc123@123abc.com'}";
+$ee = '[{ "name": "abc123@123abc.com"}]';
 
-$ver="1.4";
+$ver="1.0";
 print("New sub test v".$ver."<br>");
 print("Sub for ".$ee["name"]."<br>");
 $aeg="ieeHKghW60O4ZhSV2icXAVbmiEE/FT3RSiNC9t0SrfE=";
@@ -23,18 +23,10 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/x-www-for
 $response = curl_exec($ch); 
 curl_close($ch);
 $response=json_decode($response,true);
-print("response from event     Code :".$response.returnCode."<br>");
-print("                    username :".$response.username."<br>");
-print("                          tx : ".$response.tx."<br>");
-print("response from event     Code :".$response.["returnCode"]."<br>");
-print("                    username :".$response.["username"]."<br>");
-print("                          tx : ".$response.["tx"]."<br>");
+
 print("response from event     Code :".$response["returnCode"]."<br>");
 print("                    username :".$response["username"]."<br>");
 print("                          tx : ".$response["tx"]."<br>");
-print("response from event     Code :".$response[0]."<br>");
-print("                    username :".$response[1]."<br>");
-print("                          tx : ".$response[2]."<br>");
 var_dump($response);
 
 ?>
