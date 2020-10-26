fa5692016ba42fd60f4424cb36b00816cbb2a39e
chrisnote101 <71943715+chrisnote101@users.noreply.github.com>
2020-10-26 13:13:42 +0100

    Update ns.php

--- ns.php
+++ ns.php
@@ -5,7 +5,7 @@ class myObj {
 }
 $ee = "{ 'name': 'abc123@123abc.com'}";
 
-$ver="1.0";
+$ver="1.1";
 print("Hello world <br>");
 print("Complete ".$ver."<br>");
 $aeg="ieeHKghW60O4ZhSV2icXAVbmiEE/FT3RSiNC9t0SrfE=";
@@ -17,9 +17,9 @@ curl_setopt($ch, CURLOPT_TIMEOUT, 4);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS,$ee);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json' ,'Content-Length: ' . strlen(json_encode($arr)))   );  
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json' ,'Content-Length: ' . strlen(json_encode($arr)))   );  
 //curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'SharedAccessKeyName: chrisM','SharedAccessKey: rBJ3q5PsKatDEtltsMIV5bIL8JjNewgAuMuYv88hwHg=', 'Content-Type: application/x-www-form-urlencoded' ,'Content-Length: ' . strlen($ee))   );  
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/x-www-form-urlencoded' ,'Content-Length: ' . strlen($ee))   );  
+curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/x-www-form-urlencoded' ,'Content-Length: ' . strlen($ee))   );  
 $response = curl_exec($ch); 
 curl_close($ch);
 $response=json_decode($response,true);
